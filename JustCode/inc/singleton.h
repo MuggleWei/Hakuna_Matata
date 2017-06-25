9eed34dde73425461f119cf34a594fe9bf2315af
Muggle Wei <mugglewei@gmail.com>
2017-06-26 00:44:43 +0800

    update singleton with c++11 variadic templates

--- JustCode/inc/singleton.h
+++ JustCode/inc/singleton.h
@@ -43,6 +43,7 @@ private:
 */
 
 // version 3 - consider lifetime manager and thread safe
+/*
 template<typename T>
 class Singleton
 {
@@ -55,6 +56,55 @@ public:
 			if (singleton_ == NULL)
 			{
 				singleton_ = new T();
+				atexit(Destroy);
+			}
+		}
+
+		return *singleton_;
+	}
+
+protected:
+	Singleton() {}
+	virtual ~Singleton() {}
+
+private:
+	Singleton(const Singleton& rhs) {}
+	Singleton& operator=(const Singleton& rhs) {}
+
+	static void Destroy()
+	{
+		if (singleton_ != NULL)
+		{
+			delete singleton_;
+			singleton_ = NULL;
+		}
+	}
+
+private:
+	static T* volatile singleton_;
+	static Mutex mutex_;
+};
+
+template<typename T>
+T* volatile Singleton<T>::singleton_ = NULL;
+template<typename T>
+Mutex Singleton<T>::mutex_;
+*/
+
+// version 4 - consider constructor and use c++11 variadic templates
+template<typename T>
+class Singleton
+{
+public:
+	template<typename... Args>
+	static T& Instance(Args&&... args)
+	{
+		if (singleton_ == NULL)
+		{
+			ScopedLock lock(mutex_);
+			if (singleton_ == NULL)
+			{
+				singleton_ = new T(std::forward<Args>(args)...);
 				atexit(Destroy);
 			}
 		}
