c45c7b0c8e380bd710035d6202754f5a1f87af90
Muggle Wei <mugglewei@gmail.com>
2017-08-24 23:35:21 +0800

    add use libevent example

---
+++ use/c/use-libevent_2.1.8/src/echo_server/echo_server.c
@@ -0,0 +1,142 @@
+#include <event2/event.h>
+#include <event2/buffer.h>
+#include <event2/bufferevent.h>
+
+#if WIN32
+#pragma comment(lib, "ws2_32.lib")
+#endif
+
+void readcb(struct bufferevent *bev, void *ctx)
+{
+	char buf[128];
+	size_t n;
+
+	while (1)
+	{
+		n = bufferevent_read(bev, buf, sizeof(buf));
+		if (n <= 0)
+		{
+			break;
+		}
+		bufferevent_write(bev, buf, n);
+	}
+}
+
+void errorcb(struct bufferevent *bev, short error, void *ctx)
+{
+	if (error & BEV_EVENT_EOF)
+	{
+		printf("event eof\n");
+	}
+	else if (error & BEV_EVENT_ERROR)
+	{
+		printf("event error\n");
+	}
+	else if (error & BEV_EVENT_TIMEOUT)
+	{
+		printf("event timeout\n");
+	}
+
+	bufferevent_free(bev);
+}
+
+void do_accept(evutil_socket_t listener, short event, void *arg)
+{
+	struct event_base *base = arg;
+	struct sockaddr_storage ss;
+	socklen_t slen = sizeof(ss);
+	evutil_socket_t fd = accept(listener, (struct sockaddr*)&ss, &slen);
+	if (fd < 0)
+	{
+		perror("accept");
+	}
+	else
+	{
+		struct bufferevent *bev;
+		evutil_make_socket_nonblocking(fd);
+		bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+		bufferevent_setcb(bev, readcb, NULL, errorcb, NULL);
+		bufferevent_setwatermark(bev, EV_READ, 0, 1024 * 1024 * 2);
+		bufferevent_enable(bev, EV_READ | EV_WRITE);
+
+		fprintf(stdout, "client connected\n");
+	}
+}
+
+void run(void)
+{
+	evutil_socket_t listener;
+	struct sockaddr_in sin;
+	struct event_base *base = NULL;
+	struct event *listener_event = NULL;
+
+	memset(&sin, 0, sizeof(sin));
+
+	base = event_base_new();
+	if (!base)
+	{
+		fprintf(stderr, "Failed in event_base_new()\n");
+		return;
+	}
+
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = 0;
+	sin.sin_port = htons(40713);
+
+	listener = socket(AF_INET, SOCK_STREAM, 0);
+	evutil_make_socket_nonblocking(listener);
+
+#ifndef WIN32
+	{
+		int one = 1;
+		setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+	}
+#endif
+
+	if (bind(listener, (struct sockaddr*)&sin, sizeof(sin)) < 0)
+	{
+		perror("bind");
+		return;
+	}
+
+	if (listen(listener, 16) < 0)
+	{
+		perror("listen");
+		return;
+	}
+
+	listener_event = event_new(base, listener, EV_READ | EV_PERSIST, do_accept, (void*)base);
+	event_add(listener_event, NULL);
+
+	event_base_loop(base, EVLOOP_NO_EXIT_ON_EMPTY);
+}
+
+int main(int argc, char *argv[])
+{
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+#if WIN32
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+
+	/* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+	wVersionRequested = MAKEWORD(2, 2);
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		/* Tell the user that we could not find a usable */
+		/* Winsock DLL.                                  */
+		printf("WSAStartup failed with error: %d\n", err);
+		return 1;
+	}
+#endif
+
+	run();
+
+#if WIN32
+	WSACleanup();
+#endif
+
+	return 0;
+}
---
+++ use/c/use-libevent_2.1.8/src/simple_client/simple_client.c
@@ -0,0 +1,152 @@
+#include <event2/event.h>
+#include <event2/buffer.h>
+#include <event2/bufferevent.h>
+
+#if WIN32
+#pragma comment(lib, "ws2_32.lib")
+#endif
+
+struct event *time_task_ev = NULL;
+
+void readcb(struct bufferevent *bev, void *ctx)
+{
+	char buf[128];
+	size_t n;
+
+	while (1)
+	{
+		n = bufferevent_read(bev, buf, sizeof(buf));
+		if (n <= 0)
+		{
+			break;
+		}
+
+		for (int i = 0; i < n; ++i)
+		{
+			printf("%c", buf[i]);
+		}
+	}
+}
+
+void cb_func(evutil_socket_t fd, short what, void *arg)
+{
+	char msg[256];
+	struct timeval tv;
+
+	if (evutil_gettimeofday(&tv, NULL) == 0)
+	{
+		snprintf(msg, sizeof(msg), "hello world: %ld.%06ld\n", tv.tv_sec, tv.tv_usec);
+		struct bufferevent *bev = (struct bufferevent*)arg;
+		bufferevent_write(bev, msg, strlen(msg));
+	}
+}
+
+void eventcb(struct bufferevent *bev, short events, void *ctx)
+{
+	if (events & BEV_EVENT_CONNECTED)
+	{
+		printf("connected\n");
+
+		struct timeval time_interval = { 0,500000 };
+
+		evutil_socket_t fd = bufferevent_getfd(bev);
+		struct event_base *base = bufferevent_get_base(bev);
+		int ret = 0;
+
+		time_task_ev = event_new(base, -1, EV_PERSIST, cb_func, (void*)bev);
+		if (!time_task_ev)
+		{
+			fprintf(stderr, "failed create time task event\n");
+			return;	
+		}
+
+		ret = event_add(time_task_ev, &time_interval);
+		if (ret < 0)
+		{
+			fprintf(stderr, "failed event_add time_task_ev\n");
+			return;
+		}
+	}
+	else if (events & BEV_EVENT_EOF)
+	{
+		printf("event eof\n");
+	}
+	else if (events & BEV_EVENT_ERROR)
+	{
+		printf("event error\n");
+
+		if (time_task_ev)
+		{
+			event_free(time_task_ev);
+		}
+		bufferevent_free(bev);
+	}
+	else if (events & BEV_EVENT_TIMEOUT)
+	{
+		printf("event timeout\n");
+	}
+}
+
+void run()
+{
+	struct event_base *base = NULL;
+	struct bufferevent *bev = NULL;
+	struct sockaddr_storage ss;
+	int sockaddr_len = (int)sizeof(ss);
+
+	memset(&ss, 0, sizeof(ss));
+
+	base = event_base_new();
+
+	if (evutil_parse_sockaddr_port("127.0.0.1:40713", (struct sockaddr*)&ss, &sockaddr_len) < 0)
+	{
+		return;
+	}
+
+	bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
+	if (!bev)
+	{
+		return;
+	}
+
+	bufferevent_setcb(bev, readcb, NULL, eventcb, NULL);
+	bufferevent_enable(bev, EV_READ | EV_WRITE);
+
+	if (bufferevent_socket_connect(bev, (struct sockaddr *)&ss, sockaddr_len) < 0)
+	{
+		bufferevent_free(bev);
+		return;
+	}
+
+	event_base_loop(base, EVLOOP_NO_EXIT_ON_EMPTY);
+}
+
+int main(int argc, char *argv[])
+{
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+#if WIN32
+	WORD wVersionRequested;
+	WSADATA wsaData;
+	int err;
+
+	/* Use the MAKEWORD(lowbyte, highbyte) macro declared in Windef.h */
+	wVersionRequested = MAKEWORD(2, 2);
+
+	err = WSAStartup(wVersionRequested, &wsaData);
+	if (err != 0) {
+		/* Tell the user that we could not find a usable */
+		/* Winsock DLL.                                  */
+		printf("WSAStartup failed with error: %d\n", err);
+		return 1;
+	}
+#endif
+
+	run();
+
+#if WIN32
+	WSACleanup();
+#endif
+
+	return 0;
+}
