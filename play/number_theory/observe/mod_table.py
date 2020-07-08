24ec54f050e52a522cb991da3fe035e08c18ef8c
mugglewei <mugglewei@gmail.com>
2020-07-08 09:57:57 +0800

    update number_theory, add mod_table

---
+++ play/number_theory/observe/mod_table.py
@@ -0,0 +1,36 @@
+"""
+∀p∈素数集合, a∈N, 观察 a**k (mod p) 的结果
+"""
+import os
+
+if __name__ == '__main__':
+    p_list = [3, 5, 7, 11, 13, 17]
+    a_list = [x for x in range(1, 10)]
+    k_list = [x for x in range(1, 10)]
+
+    # 生成表
+    tables = {}
+    for p in p_list:
+        table = []
+        for a in a_list:
+            row = []
+            for k in k_list:
+                row.append((a ** k) % p)
+            table.append(row)
+        tables[p] = table
+
+    # 写入文件
+    if not os.path.exists('log'):
+        os.mkdir('log')
+    for p, table in tables.items():
+        with open('log/a_power_k_mod_{}.csv'.format(p), 'w') as f:
+            f.write('p={}|a^k(mod p),'.format(p))
+            for k in k_list:
+                f.write('a^{},'.format(k))
+            f.write('\n')
+            for i in range(len(table)):
+                f.write('a={},'.format(a_list[i]))
+                row = table[i]
+                for col in row:
+                    f.write('{},'.format(col))
+                f.write('\n')
