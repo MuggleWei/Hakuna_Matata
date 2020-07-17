41ccca438f20c243885df8dbb8a10b135b7677f3
Muggle Wei <mugglewei@gmail.com>
2020-07-17 23:02:15 +0800

    update dc datasource-services, customize tomcat MaxKeepAliveRequests

---
+++ play/distributed_calc/datasource-services/dc-user/src/main/java/com/muggle/dcuser/config/TomcatCustomizer.java
@@ -0,0 +1,38 @@
+package com.muggle.dcuser.config;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> {
+            AbstractHttp11Protocol protocol = (AbstractHttp11Protocol) connector.getProtocolHandler();
+
+            int originMaxKeepAliveRequests = protocol.getMaxKeepAliveRequests();
+            int originKeepAliveTimeout = protocol.getKeepAliveTimeout();
+            int originConnectionTimeout = protocol.getConnectionTimeout();
+            int originMaxConnection = protocol.getMaxConnections();
+            protocol.setMaxKeepAliveRequests(-1);
+
+            logger.info("####################################################################################");
+            logger.info("#");
+            logger.info("# TomcatCustomizer");
+            logger.info("#");
+            logger.info("# origin maxKeepAliveRequests: {}", originMaxKeepAliveRequests);
+            logger.info("# origin keepalive timeout: {} ms", originKeepAliveTimeout);
+            logger.info("# origin connection timeout: {} ms", originConnectionTimeout);
+            logger.info("# origin max connections: {}", originMaxConnection);
+            logger.info("# custom maxKeepAliveRequests: {} ms", protocol.getMaxKeepAliveRequests());
+            logger.info("#");
+            logger.info("####################################################################################");
+        });
+    }
+}
