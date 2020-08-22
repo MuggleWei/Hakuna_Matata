b5aa80c2206d15aab6bd4f3d29013bcbd940dc33
Muggle Wei <mugglewei@gmail.com>
2020-08-23 00:17:10 +0800

    add python/time_task

---
+++ use/python/time_task/time_task_manager.py
@@ -0,0 +1,58 @@
+import collections
+import datetime
+import logging
+import time
+
+
+class TimeTaskManager:
+    """
+    定时任务管理者
+    """
+
+    def __init__(self, tzinfo):
+        """
+        初始化定时任务管理者
+        :param tzinfo: 时区信息, 为None代表使用UTC+0时区
+        """
+        self.task_callback = {}
+        self.time_task_dict = {}
+        if tzinfo is None:
+            tzinfo = datetime.timezone(datetime.timedelta(hours=0))
+        self.tzinfo = tzinfo
+
+    def add_task(self, task_name, callback, daily_time_list):
+        self.task_callback[task_name] = callback
+        for daily_time in daily_time_list:
+            if daily_time not in self.time_task_dict:
+                self.time_task_dict[daily_time] = []
+            self.time_task_dict[daily_time].append(task_name)
+
+    def run(self):
+        logging.info('-----------------------------------')
+        logging.info('time task list: ')
+        time_task_items = self.time_task_dict.items()
+        od = collections.OrderedDict(sorted(time_task_items))
+        for k, v in od.items():
+            logging.info('{}  {}'.format(k, v))
+
+        logging.info('-----------------------------------')
+        logging.info('run time task manager')
+        last_hm = self.get_time_hm(time.time() - 120)
+        while True:
+            hm = self.get_time_hm(time.time())
+            if hm == last_hm:
+                continue
+            else:
+                last_hm = hm
+            if hm in self.time_task_dict.keys():
+                for task_name in self.time_task_dict[hm]:
+                    callback = self.task_callback.get(task_name, None)
+                    if callback is None:
+                        logging.error('failed find callback function for {}'.format(task_name))
+                    logging.info('{}  run time task {}'.format(hm, task_name))
+                    callback()
+            time.sleep(5)
+
+    def get_time_hm(self, ts):
+        dt = datetime.datetime.fromtimestamp(ts, tz=self.tzinfo)
+        return dt.strftime('%H:%M')
---
+++ use/python/time_task/time_tasks.py
@@ -0,0 +1,31 @@
+import datetime
+import logging.config
+import os
+import time
+from pathlib import Path
+
+from time_task_manager import TimeTaskManager
+
+
+def say_hello():
+    tzinfo = datetime.timezone(datetime.timedelta(hours=8))
+    ts = time.time()
+    dt_utc8 = datetime.datetime.fromtimestamp(ts, tz=tzinfo)
+    dt_utc = datetime.datetime.utcfromtimestamp(ts)
+    print("hello [UTC]{} | [UTC+8]{}".format(dt_utc, dt_utc8))
+
+
+if __name__ == '__main__':
+    if not os.path.exists("./log"):
+        os.mkdir("./log")
+    logging.config.fileConfig("config/time_tasks_log.cfg")
+
+    tzinfo = datetime.timezone(datetime.timedelta(hours=8))
+    time_task_manager = TimeTaskManager(tzinfo=tzinfo)
+
+    cur_ts = time.time()
+    hm_list = []
+    for i in range(60):
+        hm_list.append(time_task_manager.get_time_hm(cur_ts + i * 60))
+    time_task_manager.add_task('say_hello', say_hello, hm_list)
+    time_task_manager.run()
