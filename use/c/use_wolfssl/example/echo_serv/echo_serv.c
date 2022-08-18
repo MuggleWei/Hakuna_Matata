7ca8bac87cbb8691a7db0fb6d0753db2d1b41e3d
Muggle Wei <mugglewei@gmail.com>
2022-08-18 22:28:34 +0800

    update use/c/use_wolfssl: update mugglec version

--- use/c/use_wolfssl/example/echo_serv/echo_serv.c
+++ use/c/use_wolfssl/example/echo_serv/echo_serv.c
@@ -1,182 +1,17 @@
 #include "muggle/c/muggle_c.h"
 #include "wolfssl/ssl.h"
 
-typedef struct user_ev_data
+typedef struct evloop_data
 {
-	WOLFSSL_CTX *ctx;
-} user_ev_data_t;
+	WOLFSSL_CTX *ssl_ctx;
+} evloop_data_t;
 
-typedef struct user_peer_data
+typedef struct socket_ctx_data
 {
+	char remote_addr[MUGGLE_SOCKET_ADDR_STRLEN];
 	WOLFSSL *ssl;
 	int     ssl_established;  // is SSL established done
-} user_peer_data_t;
-
-/**
- * @brief  try established ssl
- *
- * @param ssl
- *
- * @return 
- *     -1: failure
- *     0: success
- *     otherwise: wait to established done
- */
-int ssl_accept(WOLFSSL *ssl)
-{
-	int ret = wolfSSL_accept(ssl);
-	int err = wolfSSL_get_error(ssl, ret);
-	if (err == WOLFSSL_ERROR_WANT_READ || err == WOLFSSL_ERROR_WANT_WRITE)
-	{
-		return 1;
-	}
-
-	if (ret == SSL_SUCCESS)
-	{
-		return 0;
-	}
-	else
-	{
-		LOG_ERROR("failed ssl accept, err=%d", err);
-		return -1;
-	}
-}
-
-void on_connect(struct muggle_socket_event *ev, struct muggle_socket_peer *listen_peer, struct muggle_socket_peer *peer)
-{
-	LOG_INFO("on connect");
-
-	user_ev_data_t *ev_data = (user_ev_data_t*)ev->datas;
-
-	// create peer data
-	user_peer_data_t *peer_data = (user_peer_data_t*)malloc(sizeof(user_peer_data_t));
-	if (peer_data == NULL)
-	{
-		LOG_ERROR("failed malloc peer data");
-		muggle_socket_peer_close(peer);
-		return;
-	}
-	peer->data = (void*)peer_data;
-	memset(peer_data, 0, sizeof(*peer_data));
-
-	// create WOLFSSL object
-	WOLFSSL *ssl = wolfSSL_new(ev_data->ctx);
-	if (ssl == NULL)
-	{
-		LOG_ERROR("failed new connect ssl");
-		muggle_socket_peer_close(peer);
-		return;
-	}
-	peer_data->ssl = ssl;
-
-	// attach wolfSSL to the socket
-	int ret = wolfSSL_set_fd(ssl, peer->fd);
-	if (ret != SSL_SUCCESS)
-	{
-		LOG_ERROR("failed set ssl fd");
-		muggle_socket_peer_close(peer);
-		return;
-	}
-
-	// try ssl accept
-	peer_data->ssl_established = 0;
-}
-
-void on_error(struct muggle_socket_event *ev, struct muggle_socket_peer *peer)
-{
-	char straddr[MUGGLE_SOCKET_ADDR_STRLEN];
-	if (peer->addr_len == 0 ||
-		muggle_socket_ntop((struct sockaddr*)&peer->addr, straddr, MUGGLE_SOCKET_ADDR_STRLEN, 0) == NULL)
-	{
-		snprintf(straddr, MUGGLE_SOCKET_ADDR_STRLEN, "?:?");
-	}
-	LOG_ERROR("on disconnect, %s", straddr);
-}
-
-void on_close(struct muggle_socket_event *ev, struct muggle_socket_peer *peer)
-{
-	char straddr[MUGGLE_SOCKET_ADDR_STRLEN];
-	if (peer->addr_len == 0 ||
-		muggle_socket_ntop((struct sockaddr*)&peer->addr, straddr, MUGGLE_SOCKET_ADDR_STRLEN, 0) == NULL)
-	{
-		snprintf(straddr, MUGGLE_SOCKET_ADDR_STRLEN, "?:?");
-	}
-	LOG_ERROR("on close, %s", straddr);
-
-	// free user data
-	user_peer_data_t *peer_data = peer->data;
-	if (peer_data)
-	{
-		if (peer_data->ssl)
-		{
-			wolfSSL_free(peer_data->ssl);
-		}
-
-		free(peer_data);
-		peer->data = NULL;
-	}
-}
-
-void on_message(struct muggle_socket_event *ev, struct muggle_socket_peer *peer)
-{
-	user_peer_data_t *peer_data = peer->data;
-	if (peer_data == NULL)
-	{
-		return;
-	}
-	WOLFSSL *ssl = peer_data->ssl;
-
-	// ensure ssl accept done
-	if (!peer_data->ssl_established)
-	{
-		LOG_INFO("on message, try ssl accept");
-
-		// try ssl accept
-		int ret = ssl_accept(ssl);
-		if (ret == 0)
-		{
-			LOG_INFO("ssl accept done");
-			peer_data->ssl_established = 1;
-		}
-		else if (ret == -1)
-		{
-			LOG_ERROR("failed ssl accept");
-			muggle_socket_peer_close(peer);
-			return;
-		}
-		else
-		{
-			// wait ssl accept done
-			LOG_INFO("wait for ssl accept done");
-			return;
-		}
-	}
-
-	// echo message
-	char buf[512];
-	memset(buf, 0, sizeof(buf));
-	do {
-		if (wolfSSL_read(ssl, buf, sizeof(buf)-1) == -1)
-		{
-			if (MUGGLE_SOCKET_LAST_ERRNO != MUGGLE_SYS_ERRNO_WOULDBLOCK)
-			{
-				LOG_ERROR("failed wolfssl read");
-				muggle_socket_peer_close(peer);
-			}
-			break;
-		}
-
-		LOG_INFO("on message: %s", buf);
-
-		size_t len = strlen(buf);
-		if (wolfSSL_write(ssl, buf, len) != len)
-		{
-			LOG_ERROR("failed wolfssl write");
-			muggle_socket_peer_close(peer);
-			break;
-		}
-	} while(1);
-}
+} socket_ctx_data_t;
 
 WOLFSSL_CTX* gen_ssl_ctx(const char *cert_file, const char *key_file)
 {
@@ -219,10 +54,212 @@ init_ssl_ctx_except:
 	return NULL;
 }
 
+/**
+ * @brief  try established ssl
+ *
+ * @param ssl
+ *
+ * @return 
+ *     -1: failure
+ *     0: success
+ *     otherwise: wait to established done
+ */
+int ssl_accept(WOLFSSL *ssl)
+{
+	int ret = wolfSSL_accept(ssl);
+	int err = wolfSSL_get_error(ssl, ret);
+	if (err == WOLFSSL_ERROR_WANT_READ || err == WOLFSSL_ERROR_WANT_WRITE)
+	{
+		return 1;
+	}
+
+	if (ret == SSL_SUCCESS)
+	{
+		return 0;
+	}
+	else
+	{
+		LOG_ERROR("failed ssl accept, err=%d", err);
+		return -1;
+	}
+}
+
+void do_ssl_accept(muggle_socket_context_t *ctx, socket_ctx_data_t *ctx_data)
+{
+	LOG_INFO("do ssl accept: %s", ctx_data->remote_addr);
+
+	WOLFSSL *ssl = ctx_data->ssl;
+	int ret = ssl_accept(ssl);
+	if (ret == 0)
+	{
+		LOG_INFO("ssl accept done");
+		ctx_data->ssl_established = 1;
+	}
+	else if (ret == -1)
+	{
+		LOG_ERROR("failed ssl accept: %s", ctx_data->remote_addr);
+		muggle_socket_ctx_shutdown(ctx);
+	}
+	else
+	{
+		LOG_INFO("ssl accept established not finish yet");
+	}
+}
+
+
+muggle_socket_context_t* tcp_listen(const char *host, const char *serv)
+{
+	// tcp listen
+	muggle_socket_t listen_fd = MUGGLE_INVALID_SOCKET;
+	do {
+		listen_fd = muggle_tcp_listen(host, serv, 512);
+		if (listen_fd == MUGGLE_INVALID_SOCKET)
+		{
+			LOG_ERROR("failed create tcp listen for %s:%s", host, serv);
+			muggle_msleep(3000);
+		}
+	} while(listen_fd == MUGGLE_INVALID_SOCKET);
+	LOG_INFO("success listen %s %s", host, serv);
+
+	// new context
+	muggle_socket_context_t *listen_ctx =
+		(muggle_socket_context_t*)malloc(sizeof(muggle_socket_context_t));
+	if (muggle_socket_ctx_init(listen_ctx, listen_fd, NULL, MUGGLE_SOCKET_CTX_TYPE_TCP_LISTEN) != 0)
+	{
+		muggle_socket_close(listen_fd);
+		LOG_ERROR("failed create socket context");
+		return NULL;
+	}
+
+	return listen_ctx;
+}
+
+void on_connect(muggle_event_loop_t *evloop, muggle_socket_context_t *ctx)
+{
+	// get remote address
+	socket_ctx_data_t *ctx_data = (socket_ctx_data_t*)malloc(sizeof(socket_ctx_data_t));
+	memset(ctx_data, 0, sizeof(*ctx_data));
+	muggle_socket_remote_addr(ctx->base.fd, ctx_data->remote_addr, sizeof(ctx_data->remote_addr), 0);
+
+	LOG_INFO("on connect: %s", ctx_data->remote_addr);
+
+	// create ssl object
+	evloop_data_t *ev_data = muggle_evloop_get_data(evloop);
+	WOLFSSL *ssl = wolfSSL_new(ev_data->ssl_ctx);
+	if (ssl == NULL)
+	{
+		LOG_ERROR("failed new connect ssl: %s", ctx_data->remote_addr);
+		muggle_socket_ctx_shutdown(ctx);
+		free(ctx_data);
+		return;
+	}
+	ctx_data->ssl = ssl;
+
+	// attach ssl to the socket
+	int ret = wolfSSL_set_fd(ssl, ctx->base.fd);
+	if (ret != SSL_SUCCESS)
+	{
+		LOG_ERROR("failed set ssl fd: %s", ctx_data->remote_addr);
+		muggle_socket_ctx_shutdown(ctx);
+		free(ctx_data);
+		return;
+	}
+	ctx_data->ssl_established = 0;
+
+	// store socket context data
+	muggle_socket_ctx_set_data(ctx, ctx_data);
+}
+
+void on_message(muggle_event_loop_t *evloop, muggle_socket_context_t *ctx)
+{
+	socket_ctx_data_t *ctx_data = muggle_socket_ctx_get_data(ctx);
+	if (ctx_data == NULL)
+	{
+		MUGGLE_ASSERT_MSG(ctx_data != NULL, "socket ctx data is NULL!");
+		return;
+	}
+
+	// ensure ssl accept done
+	if (!ctx_data->ssl_established)
+	{
+		do_ssl_accept(ctx, ctx_data);
+		if (!ctx_data->ssl_established)
+		{
+			return;
+		}
+	}
+
+	// echo message
+	WOLFSSL *ssl = ctx_data->ssl;
+	char buf[512];
+	memset(buf, 0, sizeof(buf));
+	do {
+		if (wolfSSL_read(ssl, buf, sizeof(buf)-1) == -1)
+		{
+			if (MUGGLE_SOCKET_LAST_ERRNO != MUGGLE_SYS_ERRNO_WOULDBLOCK)
+			{
+				LOG_ERROR("failed wolfssl read");
+				muggle_socket_ctx_shutdown(ctx);
+			}
+			break;
+		}
+
+		LOG_INFO("on message: %s", buf);
+
+		size_t len = strlen(buf);
+		if (wolfSSL_write(ssl, buf, len) != len)
+		{
+			LOG_ERROR("failed wolfssl write");
+			muggle_socket_ctx_shutdown(ctx);
+			break;
+		}
+	} while(1);
+}
+
+void on_close(muggle_event_loop_t *evloop, muggle_socket_context_t *ctx)
+{
+	socket_ctx_data_t *ctx_data = muggle_socket_ctx_get_data(ctx);
+	if (ctx_data == NULL)
+	{
+		MUGGLE_ASSERT_MSG(ctx_data != NULL, "socket ctx data is NULL!");
+		return;
+	}
+
+	LOG_INFO("on close: %s", ctx_data->remote_addr);
+}
+
+void on_release(muggle_event_loop_t *evloop, muggle_socket_context_t *ctx)
+{
+	socket_ctx_data_t *ctx_data = muggle_socket_ctx_get_data(ctx);
+	if (ctx_data == NULL)
+	{
+		return;
+	}
+
+	LOG_INFO("on release: %s", ctx_data->remote_addr);
+
+	if (ctx_data->ssl)
+	{
+		wolfSSL_free(ctx_data->ssl);
+	}
+	free(ctx_data);
+
+	muggle_socket_ctx_set_data(ctx, NULL);
+}
+
 int main(int argc, char *argv[])
 {
 	// initialize log
 	muggle_log_complicated_init(LOG_LEVEL_DEBUG, -1, NULL);
+
+	// parse input arguments
+	if (argc < 3)
+	{
+		LOG_ERROR("usage: %s host port", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	const char *host = argv[1];
+	const char *serv = argv[2];
 
 	// // enable wolf debug message
 	// wolfSSL_Debugging_ON();
@@ -230,63 +267,67 @@ int main(int argc, char *argv[])
 	// initialize socket lib
 	muggle_socket_lib_init();
 
-	if (argc < 3)
-	{
-		LOG_ERROR("usage: %s host port", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
-	// create listen socket
-	const char *host = argv[1];
-	const char *serv = argv[2];
-
-	muggle_socket_peer_t peer;
-	if (muggle_tcp_listen(host, serv, 512, &peer) == MUGGLE_INVALID_SOCKET)
-	{
-		MUGGLE_LOG_SYS_ERR(LOG_LEVEL_ERROR, "failed listen");
-		LOG_ERROR("failed create tcp listen for %s:%s", host, serv);
-		exit(EXIT_FAILURE);
-	}
-
 	// init ssl
 	wolfSSL_Init();
 
+	// init event loop
+	muggle_event_loop_init_args_t ev_init_args;
+	memset(&ev_init_args, 0, sizeof(ev_init_args));
+	ev_init_args.evloop_type = MUGGLE_EVLOOP_TYPE_NULL;
+	ev_init_args.hints_max_fd = 32;
+	ev_init_args.use_mem_pool = 0;
+
+	muggle_event_loop_t *evloop = muggle_evloop_new(&ev_init_args);
+	if (evloop == NULL)
+	{
+		LOG_ERROR("failed new event loop");
+		exit(EXIT_FAILURE);
+	}
+	LOG_INFO("success new event loop");
+
+	// init socket event loop handle
+	muggle_socket_evloop_handle_t handle;
+	muggle_socket_evloop_handle_init(&handle);
+	muggle_socket_evloop_handle_set_cb_conn(&handle, on_connect);
+	muggle_socket_evloop_handle_set_cb_msg(&handle, on_message);
+	muggle_socket_evloop_handle_set_cb_close(&handle, on_close);
+	muggle_socket_evloop_handle_set_cb_release(&handle, on_release);
+	muggle_socket_evloop_handle_attach(&handle, evloop);
+	LOG_INFO("socket handle attached event loop");
+
+	// create tcp listen socket and add into socket event loop
+	muggle_socket_context_t *tcp_listen_ctx = tcp_listen(host, serv);
+	muggle_socket_evloop_add_ctx(evloop, tcp_listen_ctx);
+
+	// create ssl context
 	const char *cert_file = "certs/server.crt";
 	const char *key_file = "certs/server.key";
-	WOLFSSL_CTX *ctx = gen_ssl_ctx(cert_file, key_file);
-	if (ctx == NULL)
+	WOLFSSL_CTX *ssl_ctx = gen_ssl_ctx(cert_file, key_file);
+	if (ssl_ctx == NULL)
 	{
 		LOG_ERROR("failed generate ssl ctx");
 		exit(EXIT_FAILURE);
 	}
 
-	user_ev_data_t data;
+	// set event loop data
+	evloop_data_t data;
 	memset(&data, 0, sizeof(data));
-	data.ctx = ctx;
+	data.ssl_ctx = ssl_ctx;
+	muggle_evloop_set_data(evloop, &data);
 
-	// fillup event
-	muggle_socket_event_init_arg_t ev_init_arg;
-	memset(&ev_init_arg, 0, sizeof(ev_init_arg));
-	ev_init_arg.cnt_peer = 1;
-	ev_init_arg.peers = &peer;
-	ev_init_arg.on_connect = on_connect;
-	ev_init_arg.on_error = on_error;
-	ev_init_arg.on_close = on_close;
-	ev_init_arg.on_message = on_message;
-	ev_init_arg.datas = &data;
-
-	// start event loop
-	muggle_socket_event_t ev;
-	if (muggle_socket_event_init(&ev_init_arg, &ev) != 0)
-	{
-		LOG_ERROR("failed init socket event");
-		exit(EXIT_FAILURE);
-	}
-	muggle_socket_event_loop(&ev);
+	// run event loop
+	LOG_INFO("run event loop");
+	muggle_evloop_run(evloop);
 
 	// cleanup ssl
-	wolfSSL_CTX_free(ctx);
+	wolfSSL_CTX_free(ssl_ctx);
 	wolfSSL_Cleanup();
-	
+
+	// destroy socket event loop handle
+	muggle_socket_evloop_handle_destroy(&handle);
+
+	// delete event loop
+	muggle_evloop_delete(evloop);
+
 	return 0;
 }
--- use/c/use_wolfssl/example/timer_client/timer_client.c
+++ use/c/use_wolfssl/example/timer_client/timer_client.c
@@ -1,245 +1,60 @@
 #include "muggle/c/muggle_c.h"
 #include "wolfssl/ssl.h"
 
-typedef struct user_ev_data
+typedef struct conn_thread_args
 {
-	WOLFSSL_CTX *ctx;
-	muggle_socket_peer_t *peer;
-} user_ev_data_t;
+	muggle_event_loop_t *evloop;
+	const char *host;
+	const char *port;
+} conn_thread_args_t;
 
-typedef struct user_peer_data
+typedef struct ev_data
+{
+	WOLFSSL_CTX             *ssl_ctx;
+	muggle_socket_context_t *socket_ctx;
+	conn_thread_args_t      *conn_args;
+} ev_data_t;
+
+typedef struct socket_ctx_data
 {
 	WOLFSSL *ssl;
 	int     ssl_established;  // is SSL established done
-} user_peer_data_t;
+} socket_ctx_data_t;
 
-/**
- * @brief  try established ssl
- *
- * @param ssl
- *
- * @return 
- *     -1: failure
- *     0: success
- *     otherwise: wait to established done
- */
-int ssl_connect(WOLFSSL *ssl)
+muggle_thread_ret_t conn_thread_routine(void *p_args)
 {
-	int ret = wolfSSL_connect(ssl);
-	int err = wolfSSL_get_error(ssl, ret);
-	if (err == WOLFSSL_ERROR_WANT_READ || err == WOLFSSL_ERROR_WANT_WRITE)
+	conn_thread_args_t *args = (conn_thread_args_t*)p_args;
+
+	while (1)
 	{
-		return 1;
-	}
-
-	if (ret == SSL_SUCCESS)
-	{
-		return 0;
-	}
-	else
-	{
-		LOG_ERROR("failed ssl connect, err=%d", err);
-		return -1;
-	}
-}
-
-void on_connect(struct muggle_socket_event *ev, struct muggle_socket_peer *listen_peer, struct muggle_socket_peer *peer)
-{
-	LOG_INFO("on connect");
-
-	user_ev_data_t *ev_data = (user_ev_data_t*)ev->datas;
-
-	// create peer data
-	user_peer_data_t *peer_data = (user_peer_data_t*)malloc(sizeof(user_peer_data_t));
-	if (peer_data == NULL)
-	{
-		LOG_ERROR("failed malloc peer data");
-		muggle_socket_peer_close(peer);
-		muggle_socket_event_loop_exit(ev);
-		return;
-	}
-	peer->data = (void*)peer_data;
-	memset(peer_data, 0, sizeof(*peer_data));
-
-	// create WOLFSSL object
-	WOLFSSL *ssl = wolfSSL_new(ev_data->ctx);
-	if (ssl == NULL)
-	{
-		LOG_ERROR("failed new connect ssl");
-		muggle_socket_peer_close(peer);
-		muggle_socket_event_loop_exit(ev);
-		return;
-	}
-	peer_data->ssl = ssl;
-
-	// attach wolfSSL to the socket
-	int ret = wolfSSL_set_fd(ssl, peer->fd);
-	if (ret != SSL_SUCCESS)
-	{
-		LOG_ERROR("failed set ssl fd");
-		muggle_socket_peer_close(peer);
-		muggle_socket_event_loop_exit(ev);
-		return;
-	}
-
-	// connect to wolfSSL on the server side
-	peer_data->ssl_established = 0;
-	ret = ssl_connect(ssl);
-	if (ret == 0)
-	{
-		LOG_INFO("ssl connect done");
-		peer_data->ssl_established = 1;
-	}
-	else if (ret == -1)
-	{
-		LOG_ERROR("failed connect ssl server side");
-		muggle_socket_peer_close(peer);
-		muggle_socket_event_loop_exit(ev);
-		return;
-	}
-
-	// set client peer
-	ev_data->peer = peer;
-}
-
-void on_error(struct muggle_socket_event *ev, struct muggle_socket_peer *peer)
-{
-	char straddr[MUGGLE_SOCKET_ADDR_STRLEN];
-	if (peer->addr_len == 0 ||
-		muggle_socket_ntop((struct sockaddr*)&peer->addr, straddr, MUGGLE_SOCKET_ADDR_STRLEN, 0) == NULL)
-	{
-		snprintf(straddr, MUGGLE_SOCKET_ADDR_STRLEN, "?:?");
-	}
-	LOG_ERROR("on disconnect, %s", straddr);
-
-}
-
-void on_close(struct muggle_socket_event *ev, struct muggle_socket_peer *peer)
-{
-	char straddr[MUGGLE_SOCKET_ADDR_STRLEN];
-	if (peer->addr_len == 0 ||
-		muggle_socket_ntop((struct sockaddr*)&peer->addr, straddr, MUGGLE_SOCKET_ADDR_STRLEN, 0) == NULL)
-	{
-		snprintf(straddr, MUGGLE_SOCKET_ADDR_STRLEN, "?:?");
-	}
-	LOG_ERROR("on close, %s", straddr);
-
-	// free user data
-	user_peer_data_t *peer_data = peer->data;
-	if (peer_data)
-	{
-		if (peer_data->ssl)
+		// create socket fd
+		muggle_socket_t fd = muggle_tcp_connect(args->host, args->port, 3);
+		if (fd == MUGGLE_INVALID_SOCKET)
 		{
-			wolfSSL_free(peer_data->ssl);
+			LOG_ERROR("failed connect to: %s %s, wait retry", args->host, args->port);
+			muggle_msleep(3000);
+			continue;
 		}
 
-		free(peer_data);
-		peer->data = NULL;
+		// create socket context
+		muggle_socket_context_t *ctx = (muggle_socket_context_t*)malloc(sizeof(muggle_socket_context_t));
+		muggle_socket_ctx_init(ctx, fd, NULL, MUGGLE_SOCKET_CTX_TYPE_TCP_CLIENT);
+
+		// add context to event loop
+		muggle_socket_evloop_add_ctx(args->evloop, ctx);
+
+		// exit connect thread
+		break;
 	}
 
-	// set ev_data->peer to NULL
-	user_ev_data_t *ev_data = (user_ev_data_t*)ev->datas;
-	if (ev_data->peer == NULL)
-	{
-		return;
-	}
-
-	// exit event loop
-	muggle_socket_event_loop_exit(ev);
+	return 0;
 }
 
-void on_message(struct muggle_socket_event *ev, struct muggle_socket_peer *peer)
+void run_conn_thread(conn_thread_args_t *args)
 {
-	user_peer_data_t *peer_data = peer->data;
-	if (peer_data == NULL)
-	{
-		return;
-	}
-	WOLFSSL *ssl = peer_data->ssl;
-
-	// ensure ssl connect done
-	if (!peer_data->ssl_established)
-	{
-		int ret = ssl_connect(ssl);
-		if (ret == 0)
-		{
-			LOG_INFO("ssl connect done");
-			peer_data->ssl_established = 1;
-		}
-		else if (ret == -1)
-		{
-			LOG_ERROR("failed connect ssl server side");
-			muggle_socket_peer_close(peer);
-			muggle_socket_event_loop_exit(ev);
-			return;
-		}
-		else
-		{
-			// wait ssl connect done
-			LOG_INFO("wait for ssl connect done");
-			return;
-		}
-	}
-
-	// read message
-	char buf[512];
-	memset(buf, 0, sizeof(buf));
-	do {
-		if (wolfSSL_read(ssl, buf, sizeof(buf)-1) == -1)
-		{
-			if (MUGGLE_SOCKET_LAST_ERRNO != MUGGLE_SYS_ERRNO_WOULDBLOCK)
-			{
-				LOG_ERROR("failed wolfssl read");
-				muggle_socket_peer_close(peer);
-			}
-			break;
-		}
-
-		LOG_INFO("on message: %s", buf);
-	} while(1);
-}
-
-void on_timer(struct muggle_socket_event *ev)
-{
-	// get client peer
-	user_ev_data_t *ev_data = (user_ev_data_t*)ev->datas;
-	muggle_socket_peer_t *peer = ev_data->peer;
-	if (peer == NULL)
-	{
-		return;
-	}
-
-	user_peer_data_t *peer_data = peer->data;
-	if (peer_data == NULL)
-	{
-		return;
-	}
-	WOLFSSL *ssl = peer_data->ssl;
-
-	if (!peer_data->ssl_established)
-	{
-		// LOG_INFO("on timer, wait ssl connect done");
-		return;
-	}
-
-	// get current ts
-	struct timespec ts;
-	timespec_get(&ts, TIME_UTC);
-
-	char buf[64];
-	memset(buf, 0, sizeof(buf));
-	snprintf(buf, sizeof(buf), "%llu.%03d",
-		(unsigned long long)ts.tv_sec, (int)(ts.tv_nsec / 1000000));
-
-	LOG_INFO("on timer, send message %s", buf);
-	
-	// write to server
-	size_t len = strlen(buf);
-	if (wolfSSL_write(ssl, buf, len) != len)
-	{
-		LOG_ERROR("failed wolfssl write");
-		muggle_socket_peer_close(peer);
-	}
+	muggle_thread_t th;
+	muggle_thread_create(&th, conn_thread_routine, args);
+	muggle_thread_detach(&th);
 }
 
 WOLFSSL_CTX* gen_ssl_ctx(const char *ca_file)
@@ -275,16 +90,210 @@ init_ssl_ctx_except:
 	return NULL;
 }
 
+/**
+ * @brief  try established ssl
+ *
+ * @param ssl
+ *
+ * @return 
+ *     -1: failure
+ *     0: success
+ *     otherwise: wait to established done
+ */
+int ssl_connect(WOLFSSL *ssl)
+{
+	int ret = wolfSSL_connect(ssl);
+	int err = wolfSSL_get_error(ssl, ret);
+	if (err == WOLFSSL_ERROR_WANT_READ || err == WOLFSSL_ERROR_WANT_WRITE)
+	{
+		return 1;
+	}
+
+	if (ret == SSL_SUCCESS)
+	{
+		return 0;
+	}
+	else
+	{
+		LOG_ERROR("failed ssl connect, err=%d", err);
+		return -1;
+	}
+}
+
+void do_ssl_connect(muggle_socket_context_t *ctx, socket_ctx_data_t *ctx_data)
+{
+	LOG_INFO("do ssl connect");
+
+	WOLFSSL *ssl = ctx_data->ssl;
+	int ret = ssl_connect(ssl);
+	if (ret == 0)
+	{
+		LOG_INFO("ssl connect done");
+		ctx_data->ssl_established = 1;
+	}
+	else if (ret == -1)
+	{
+		LOG_ERROR("failed connect ssl server side");
+		muggle_socket_ctx_shutdown(ctx);
+	}
+}
+
+void on_add_ctx(muggle_event_loop_t *evloop, muggle_socket_context_t *ctx)
+{
+	LOG_INFO("on add socket context into event loop");
+
+	ev_data_t *ev_data = (ev_data_t*)muggle_evloop_get_data(evloop);
+
+	// create socket ctx data
+	socket_ctx_data_t *ctx_data = (socket_ctx_data_t*)malloc(sizeof(socket_ctx_data_t));
+	memset(ctx_data, 0, sizeof(*ctx_data));
+
+	// create WOLFSSL object
+	WOLFSSL *ssl = wolfSSL_new(ev_data->ssl_ctx);
+	if (ssl == NULL)
+	{
+		LOG_ERROR("failed new connect ssl");
+		muggle_socket_ctx_shutdown(ctx);
+		free(ctx_data);
+		return;
+	}
+	ctx_data->ssl = ssl;
+
+	// attach wolfSSL to the socket
+	int ret = wolfSSL_set_fd(ssl, ctx->base.fd);
+	if (ret != SSL_SUCCESS)
+	{
+		LOG_ERROR("failed set ssl fd");
+		muggle_socket_ctx_shutdown(ctx);
+		free(ctx_data);
+		return;
+	}
+	ctx_data->ssl_established = 0;
+
+	// store socket context data
+	muggle_socket_ctx_set_data(ctx, ctx_data);
+
+	// store socket context into event loop data
+	ev_data->socket_ctx = ctx;
+
+	// connect to ssl on the server side
+	do_ssl_connect(ctx, ctx_data);
+}
+
+void on_message(muggle_event_loop_t *evloop, muggle_socket_context_t *ctx)
+{
+	socket_ctx_data_t *ctx_data = muggle_socket_ctx_get_data(ctx);
+	if (ctx_data == NULL)
+	{
+		MUGGLE_ASSERT_MSG(ctx_data != NULL, "socket ctx data is NULL!");
+		return;
+	}
+
+	// ensure ssl accept done
+	if (!ctx_data->ssl_established)
+	{
+		do_ssl_connect(ctx, ctx_data);
+		if (!ctx_data->ssl_established)
+		{
+			return;
+		}
+	}
+
+	// read message
+	WOLFSSL *ssl = ctx_data->ssl;
+	char buf[512];
+	memset(buf, 0, sizeof(buf));
+	do {
+		if (wolfSSL_read(ssl, buf, sizeof(buf)-1) == -1)
+		{
+			if (MUGGLE_SOCKET_LAST_ERRNO != MUGGLE_SYS_ERRNO_WOULDBLOCK)
+			{
+				LOG_ERROR("failed wolfssl read");
+				muggle_socket_ctx_shutdown(ctx);
+			}
+			break;
+		}
+
+		LOG_INFO("on message: %s", buf);
+	} while(1);
+}
+
+void on_timer(muggle_event_loop_t *evloop)
+{
+	ev_data_t *ev_data = muggle_evloop_get_data(evloop);
+
+	muggle_socket_context_t *ctx = ev_data->socket_ctx;
+	if (ctx == NULL)
+	{
+		return;
+	}
+
+	socket_ctx_data_t *ctx_data = muggle_socket_ctx_get_data(ctx);
+	if (ctx_data == NULL)
+	{
+		return;
+	}
+
+	if (!ctx_data->ssl_established)
+	{
+		return;
+	}
+
+	WOLFSSL *ssl = ctx_data->ssl;
+
+	// get current ts
+	struct timespec ts;
+	timespec_get(&ts, TIME_UTC);
+
+	char buf[64];
+	memset(buf, 0, sizeof(buf));
+	snprintf(buf, sizeof(buf), "%llu.%03d",
+		(unsigned long long)ts.tv_sec, (int)(ts.tv_nsec / 1000000));
+
+	LOG_INFO("on timer, send message %s", buf);
+	
+	// write to server
+	size_t len = strlen(buf);
+	if (wolfSSL_write(ssl, buf, len) != len)
+	{
+		LOG_ERROR("failed wolfssl write");
+	}
+}
+
+void on_close(muggle_event_loop_t *evloop, muggle_socket_context_t *ctx)
+{
+	LOG_WARNING("on disconnect");
+
+	ev_data_t *ev_data = (ev_data_t*)muggle_evloop_get_data(evloop);
+	ev_data->socket_ctx = NULL;
+
+	// reconnect
+	run_conn_thread(ev_data->conn_args);
+}
+
+void on_release(muggle_event_loop_t *evloop, muggle_socket_context_t *ctx)
+{
+	LOG_INFO("on release");
+
+	socket_ctx_data_t *ctx_data = muggle_socket_ctx_get_data(ctx);
+	if (ctx_data == NULL)
+	{
+		return;
+	}
+
+	if (ctx_data->ssl)
+	{
+		wolfSSL_free(ctx_data->ssl);
+	}
+	free(ctx_data);
+
+	muggle_socket_ctx_set_data(ctx, NULL);
+}
+
 int main(int argc, char *argv[])
 {
 	// initialize log
 	muggle_log_complicated_init(LOG_LEVEL_DEBUG, -1, NULL);
-
-	// // enable wolf debug message
-	// wolfSSL_Debugging_ON();
-
-	// initialize socket lib
-	muggle_socket_lib_init();
 
 	if (argc < 3)
 	{
@@ -292,73 +301,79 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	// create client socket
-	const char *host = argv[1];
-	const char *serv = argv[2];
+	// // enable wolf debug message
+	// wolfSSL_Debugging_ON();
+
+	// initialize socket lib
+	muggle_socket_lib_init();
 
 	// init ssl
 	wolfSSL_Init();
 
+	// init event loop
+	muggle_event_loop_init_args_t ev_init_args;
+	memset(&ev_init_args, 0, sizeof(ev_init_args));
+	ev_init_args.evloop_type = MUGGLE_EVLOOP_TYPE_NULL;
+	ev_init_args.hints_max_fd = 4;
+	ev_init_args.use_mem_pool = 0;
+
+	muggle_event_loop_t *evloop = muggle_evloop_new(&ev_init_args);
+	if (evloop == NULL)
+	{
+		LOG_ERROR("failed new event loop");
+		exit(EXIT_FAILURE);
+	}
+	LOG_INFO("success new event loop");
+
+	// init socket event loop handle
+	muggle_socket_evloop_handle_t handle;
+	muggle_socket_evloop_handle_init(&handle);
+	muggle_socket_evloop_handle_set_timer_interval(&handle, 3 * 1000);
+	muggle_socket_evloop_handle_set_cb_add_ctx(&handle, on_add_ctx);
+	muggle_socket_evloop_handle_set_cb_msg(&handle, on_message);
+	muggle_socket_evloop_handle_set_cb_timer(&handle, on_timer);
+	muggle_socket_evloop_handle_set_cb_close(&handle, on_close);
+	muggle_socket_evloop_handle_set_cb_release(&handle, on_release);
+	muggle_socket_evloop_handle_attach(&handle, evloop);
+	LOG_INFO("socket handle attached event loop");
+
+	// create tcp connect socket
+	conn_thread_args_t conn_args;
+	memset(&conn_args, 0, sizeof(conn_args));
+	conn_args.evloop = evloop;
+	conn_args.host = argv[1];
+	conn_args.port = argv[2];
+	run_conn_thread(&conn_args);
+
+	// create ssl context
 	const char *ca_file = "certs/ca.crt";
-	WOLFSSL_CTX *ctx = gen_ssl_ctx(ca_file);
-	if (ctx == NULL)
+	WOLFSSL_CTX *ssl_ctx = gen_ssl_ctx(ca_file);
+	if (ssl_ctx == NULL)
 	{
 		LOG_ERROR("failed generate ssl ctx");
 		exit(EXIT_FAILURE);
 	}
 
-	user_ev_data_t ev_data;
+	// set event loop data
+	ev_data_t ev_data;
 	memset(&ev_data, 0, sizeof(ev_data));
-	ev_data.ctx = ctx;
+	ev_data.ssl_ctx = ssl_ctx;
+	ev_data.conn_args = &conn_args;
+	muggle_evloop_set_data(evloop, &ev_data);
 
-	// run client event loop
-	muggle_socket_peer_t *connect_peer = NULL;
-	int connect_timeout = 3;
-	while (1)
-	{
-		muggle_socket_peer_t peer;
-		if (muggle_tcp_connect(host, serv, connect_timeout, &peer) == MUGGLE_INVALID_SOCKET)
-		{
-			MUGGLE_LOG_ERROR("failed connect %s:%s", host, serv);
-			muggle_msleep(3000);
-			continue;
-		}
-
-		// fill up event loop input arguments
-		muggle_socket_event_init_arg_t ev_init_arg;
-		memset(&ev_init_arg, 0, sizeof(ev_init_arg));
-		ev_init_arg.hints_max_peer = 1024;
-		ev_init_arg.cnt_peer = 1;
-		ev_init_arg.peers = &peer;
-		ev_init_arg.p_peers = &connect_peer;
-		ev_init_arg.timeout_ms = 1000;
-		ev_init_arg.datas = &ev_data;
-		ev_init_arg.on_connect = NULL;
-		ev_init_arg.on_error = on_error;
-		ev_init_arg.on_close = on_close;
-		ev_init_arg.on_message = on_message;
-		ev_init_arg.on_timer = on_timer;
-
-		// event loop init
-		muggle_socket_event_t ev;
-		if (muggle_socket_event_init(&ev_init_arg, &ev) != 0)
-		{
-			MUGGLE_LOG_ERROR("failed init socket event");
-			exit(EXIT_FAILURE);
-		}
-
-		// on client connection
-		on_connect(&ev, NULL, connect_peer);
-
-		// event loop
-		muggle_socket_event_loop(&ev);
-
-		muggle_msleep(3000);
-	}
+	// run event loop
+	LOG_INFO("run event loop");
+	muggle_evloop_run(evloop);
 
 	// cleanup ssl
-	wolfSSL_CTX_free(ctx);
+	wolfSSL_CTX_free(ssl_ctx);
 	wolfSSL_Cleanup();
+
+	// destroy socket event loop handle
+	muggle_socket_evloop_handle_destroy(&handle);
+
+	// delete event loop
+	muggle_evloop_delete(evloop);
 
 	return 0;
 }
