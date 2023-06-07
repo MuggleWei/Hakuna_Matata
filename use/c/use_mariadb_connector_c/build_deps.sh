b39016df63425a8794f48772d594e969138e9093
Muggle Wei <mugglewei@gmail.com>
2023-06-07 16:35:08 +0800

    update use/c/mariadb_connector_c

--- use/c/use_mariadb_connector_c/build_deps.sh
+++ use/c/use_mariadb_connector_c/build_deps.sh
@@ -10,11 +10,20 @@ else
 	BUILD_TYPE=$(echo $1 | tr '[:upper:]' '[:lower:]')
 fi
 
+case "$OSTYPE" in
+	linux*) OPENSSL_YML='unix' ;;
+	bsd*) OPENSSL_YML='unix' ;;
+	darwin*) OPENSSL_YML='unix' ;;
+	msys*) OPENSSL_YML='win' ;;
+	cygwin*) OPENSSL_YML='win' ;;
+	*) OPENSSL_YML='unix' ;;
+esac
+
 origin_dir="$(dirname "$(readlink -f "$0")")"
 
 cd $origin_dir
 
 hpb build -m task -c $origin_dir/modules/mugglec.yml -p build_type=$BUILD_TYPE
 hpb build -m task -c $origin_dir/modules/zlib.yml -p build_type=$BUILD_TYPE
-hpb build -m task -c $origin_dir/modules/openssl.yml -p build_type=$BUILD_TYPE
+hpb build -m task -c $origin_dir/modules/openssl.${OPENSSL_YML}.yml -p build_type=$BUILD_TYPE
 hpb build -m task -c $origin_dir/modules/mariadb_connector_c.yml -p build_type=$BUILD_TYPE
