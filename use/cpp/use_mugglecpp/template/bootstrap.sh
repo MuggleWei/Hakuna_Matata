0985012dfa681078a000d4a4b63196ca04326dc9
Muggle Wei <mugglewei@gmail.com>
2023-07-12 16:43:34 +0800

    update use/cpp/use_mugglecpp/template

--- use/cpp/use_mugglecpp/template/bootstrap.sh
+++ use/cpp/use_mugglecpp/template/bootstrap.sh
@@ -15,11 +15,16 @@ echo "Build Type: $BUILD_TYPE"
 origin_dir="$(dirname "$(readlink -f "$0")")"
 cd $origin_dir
 
+build_dir=$origin_dir/build
+deps_dir=$origin_dir/_deps
+install_dir=$build_dir/dist
+
 mkdir build
 cd build
-cmake .. \
+cmake -S $origin_dir -B $build_dir \
 	-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
 	-DBUILD_SHARED_LIBS=ON \
-	-DMUGGLE_BUILD_SHARED_LIB=ON \
-	-DMUGGLE_CPP_BUILD_SHARED_LIB=ON
+	-DCMAKE_INSTALL_PREFIX=$install_dir \
+	-DMUGGLE_CPP_DOWNLOAD_MUGGLEC=ON
 cmake --build .
+cmake --build . --target install
