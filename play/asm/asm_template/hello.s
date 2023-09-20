112944cac7680a5d727f917fef97c7cf8142c91b
Muggle Wei <mugglewei@gmail.com>
2023-09-20 11:22:13 +0800

    update play/asm/asm_template

--- play/asm/asm_template/hello.s
+++ play/asm/asm_template/hello.s
@@ -11,6 +11,9 @@
 # --------------------------------
 	.section .data
 
+str_hello:
+	.string "hello world\n"
+
 # --------------------------------
 # Section: text
 # --------------------------------
@@ -20,6 +23,13 @@
 _start:
 
 exit:
+	# system call: write(stdout, str_hello, 12)
+	movl $4, %eax  # system call: write
+	movl $1, %ebx  # output file descripte
+	movl $str_hello, %ecx  # get str address
+	movl $12, %edx
+	int $0x80
+
 	# system call: exit(0)
 	movl $1, %eax
 	movl $0, %ebx
