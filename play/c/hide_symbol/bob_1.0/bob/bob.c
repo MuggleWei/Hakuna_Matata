27a346f73e4c905f2707bb6061c12e657a556639
Muggle Wei <mugglewei@gmail.com>
2023-11-01 17:10:14 +0800

    update play/c/hide_symbol

--- play/c/hide_symbol/bob_1.0/bob/bob.c
+++ play/c/hide_symbol/bob_1.0/bob/bob.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include "foo/foo.h"
 
-#define BOB_VERSION "1.0.0"
-
 void bob_say_hello(const char *name)
 {
 	fprintf(stdout, "bob[%s] call | ", BOB_VERSION);
--- play/c/hide_symbol/bob_1.0/bob/bob.h
+++ play/c/hide_symbol/bob_1.0/bob/bob.h
@@ -1,6 +1,9 @@
 #ifndef BOB_H_
 #define BOB_H_
 
+#include "bob/bob_config.h"
+
+BOB_EXPORT
 void bob_say_hello(const char *name);
 
 #endif // !BOB_H_
