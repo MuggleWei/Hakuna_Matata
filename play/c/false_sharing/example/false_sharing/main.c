493b5f46246a387e1212d7208dda4370fbe1f7a5
Muggle Wei <mugglewei@gmail.com>
2025-01-09 15:36:31 +0800

    update play/c/false_sharing

--- play/c/false_sharing/example/false_sharing/main.c
+++ play/c/false_sharing/example/false_sharing/main.c
@@ -1,96 +1,128 @@
 #include "muggle/c/muggle_c.h"
 
+#if DATA_ALIGN_SIZE_128
+typedef union {
+	muggle_sync_t val;
+	MUGGLE_STRUCT_CACHE_LINE_X2_PADDING(0);
+} data_t;
+#elif DATA_ALIGN_SIZE_64
+typedef union {
+	muggle_sync_t val;
+	MUGGLE_STRUCT_CACHE_LINE_PADDING(0);
+} data_t;
+#else
 typedef struct {
-	int a;
-	int b;
-} foo_t;
+	muggle_sync_t val;
+} data_t;
+#endif
 
 typedef struct {
-	int a;
-	char cacheline[64];
-	int b;
-} bar_t;
+	int cpu;
+	muggle_sync_t *pval;
+	int64_t elapsed;
+	uint32_t *ready;
+} thread_data_t;
 
-static uint32_t iter_cnt = 10000;
-static int ready = 0;
-
-muggle_thread_ret_t increase(void *args)
+muggle_thread_ret_t atomic_increase(void *args)
 {
-	int *p = (int*)args;
+	thread_data_t *data = (thread_data_t *)args;
+	muggle_sync_t *pval = data->pval;
 
-	while (muggle_atomic_load(&ready, muggle_memory_order_relaxed) == 0) {
+	// bind cpu
+	muggle_cpu_mask_t mask;
+	muggle_cpu_mask_zero(&mask);
+	muggle_cpu_mask_set(&mask, data->cpu);
+	if (muggle_cpu_set_thread_affinity(0, &mask) != 0) {
+		fprintf(stderr, "failed bind CPU #%d\n", data->cpu);
 	}
 
-	struct timespec ts1, ts2;
-	muggle_realtime_get(ts1);
-
-	for (uint32_t i = 0; i < iter_cnt; ++i) {
-		muggle_atomic_fetch_add(p, 1, muggle_memory_order_relaxed);
+	// wait to start
+	while (muggle_atomic_load(data->ready, muggle_memory_order_relaxed) == 0) {
 	}
 
-	muggle_realtime_get(ts2);
+	// run atomic increase
+	muggle_time_counter_t tc;
+	muggle_time_counter_init(&tc);
+	muggle_time_counter_start(&tc);
+	for (uint32_t i = 0; i < (1 << 20); ++i) {
+		muggle_atomic_fetch_add(pval, 1, muggle_memory_order_relaxed);
+	}
+	muggle_time_counter_end(&tc);
 
-	unsigned long long elapsed =
-		(ts2.tv_sec - ts1.tv_sec) * 1000000000 + ts2.tv_nsec - ts1.tv_nsec;
-	LOG_INFO("increase use time: %.3f us", elapsed / 1000.0);
+	data->elapsed = muggle_time_counter_interval_ns(&tc);
 
-	return NULL;
+	return 0;
+}
+
+void run(thread_data_t *th_datas, int n, uint32_t *ready)
+{
+	muggle_thread_t *th_arr =
+		(muggle_thread_t *)malloc(sizeof(muggle_thread_t) * n);
+
+	for (int i = 0; i < n; ++i) {
+		muggle_thread_create(&th_arr[i], atomic_increase, &th_datas[i]);
+	}
+
+	muggle_msleep(100);
+	muggle_atomic_store(ready, 1, muggle_memory_order_relaxed);
+
+	for (int i = 0; i < n; ++i) {
+		muggle_thread_join(&th_arr[i]);
+	}
+
+	free(th_arr);
 }
 
 int main(int argc, char *argv[])
 {
-	iter_cnt *= argc;
-	MUGGLE_UNUSED(argv);
-
-	muggle_log_simple_init(LOG_LEVEL_DEBUG, -1);
-
-	// false sharing
-	{
-		ready = 0;
-
-		foo_t data;
-		memset(&data, 0, sizeof(data));
-
-		LOG_INFO("--------------------------------");
-		LOG_INFO("          false sharing");
-		LOG_INFO("--------------------------------");
-
-		muggle_thread_t th1, th2;
-		muggle_thread_create(&th1, increase, &data.a);
-		muggle_thread_create(&th2, increase, &data.b);
-
-		muggle_msleep(100);
-		muggle_atomic_store(&ready, 1, muggle_memory_order_relaxed);
-
-		muggle_thread_join(&th1);
-		muggle_thread_join(&th2);
-
-		// LOG_INFO("a=%d, b=%d", data.a, data.b);
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s <cpu-1> <cpu-2> [cpu-3] ... [cpu-N]\n",
+				argv[0]);
+		fprintf(stderr, "NOTE: try cpu nodes in same or different NUMA\n");
+		fprintf(stderr,
+				"e.g.\n"
+				"  perf stat -e "
+				"instructions,cycles,cache-misses,L1-dcache-load-misses,"
+				"LLC-load-misses,node-load-misses %s 0 1\n",
+				argv[0]);
+		exit(EXIT_FAILURE);
 	}
 
-	// add cache line
-	{
-		ready = 0;
+	int n = argc - 1;
+	uint32_t ready = 0;
 
-		bar_t data;
-		memset(&data, 0, sizeof(data));
-
-		LOG_INFO("--------------------------------");
-		LOG_INFO("          cache line");
-		LOG_INFO("--------------------------------");
-
-		muggle_thread_t th1, th2;
-		muggle_thread_create(&th1, increase, &data.a);
-		muggle_thread_create(&th2, increase, &data.b);
-
-		muggle_msleep(100);
-		muggle_atomic_store(&ready, 1, muggle_memory_order_relaxed);
-
-		muggle_thread_join(&th1);
-		muggle_thread_join(&th2);
-
-		// LOG_INFO("a=%d, b=%d", data.a, data.b);
+#if MUGGLE_C_HAVE_ALIGNED_ALLOC
+	data_t *datas =
+		(data_t *)aligned_alloc(MUGGLE_CACHE_LINE_X2_SIZE, sizeof(data_t) * n);
+	thread_data_t *th_datas = (thread_data_t *)aligned_alloc(
+		MUGGLE_CACHE_LINE_X2_SIZE, sizeof(thread_data_t) * n);
+#else
+	data_t *datas = (data_t *)malloc(sizeof(data_t) * n);
+	thread_data_t *th_datas = malloc(sizeof(thread_data_t) * n);
+#endif
+	for (int i = 0; i < n; ++i) {
+		th_datas[i].cpu = atoi(argv[i + 1]);
+		th_datas[i].pval = &datas[i].val;
+		th_datas[i].elapsed = 0;
+		th_datas[i].ready = &ready;
 	}
+
+	run(th_datas, n, &ready);
+
+#if DATA_ALIGN_SIZE_128
+	fprintf(stdout, "data align x2 cache line\n");
+#elif DATA_ALIGN_SIZE_64
+	fprintf(stdout, "data align cache line\n");
+#else
+	fprintf(stdout, "data without align\n");
+#endif
+	for (int i = 0; i < n; ++i) {
+		fprintf(stdout, "cpu: %d, elapsed: %lld ns\n", th_datas[i].cpu,
+				(long long)th_datas[i].elapsed);
+	}
+
+	free(th_datas);
+	free(datas);
 
 	return 0;
 }
